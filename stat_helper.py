2132c6a527d1da290cac843503a4a3029c2b2ff3
Phillip Korolev <p.korolev1@outlook.com>
2025-05-30 12:41:10 -0400

    Updated all files

--- stat_helper.py
+++ stat_helper.py
@@ -1,5 +1,9 @@
 import math, numpy as np
 
+# generate X: returns list from 1 to size
+def generate_X(size: int) -> list[int]:
+    return list(range(1, size+1))
+
 #get mean of sample
 def average(data_set: list[float]) -> float:
     avg = sum(data_set)/len(data_set)
@@ -27,10 +31,6 @@ def sd(data_set: list[float]) -> float:
     variance = variance(data_set)
     return math.sqrt(variance)
 
-# generate X: returns list from 1 to size
-def generate_X(size: int) -> list[int]:
-    return list(range(1, size+1))
-
 # get moving average given period
 def sma(data_set: list[float], period: int) -> tuple[list]:
     '''Returns tuple where tuple[0] -> X axis, tuple[1] -> Y axis'''
@@ -38,22 +38,22 @@ def sma(data_set: list[float], period: int) -> tuple[list]:
     # basic variables
     size = len(data_set)
     reversed_data = data_set[::-1]
+
+    #partition data into periods
     split = partition.partition_set(reversed_data, period)
 
-    # get moving average values
-    sma_Y = []
+    # get moving average X, Y values
+    sma_Y, sma_X = [], []
+    x_index = -1
+    day_count = 0
     for partition in split:
         sma_Y.append(average(partition))
+        day_count += len(split[x_index])
+        sma_X.append(day_count)
+        x_index -= 1
 
-    # re-reverse
+    # re-reverse moving average values 
     sma_Y = sma_Y[::-1]
-
-    # get respective X values
-    sma_X = []
-    X = generate_X(size)
-    for x in X:
-        if x%period==0:
-            sma_X.append(x)
 
     return sma_X, sma_Y
 
--- stoch_walk.py
+++ stoch_walk.py
@@ -1,11 +1,11 @@
-import math, random as R, stat_helper as sh, matplotlib.pyplot as plt
+import math, random as rd, stat_helper as sh, matplotlib.pyplot as plt
 
 class Walk():
     def __init__(self, start_val: float):
         self.size = 1                                # initial size
         self.start = start_val                       # walk start value
         self.current_walk = [start_val]              # initial walk path (start value)
-        self.current_value = start_val   # initial last appended step position
+        self.current_value = start_val               # initial last appended step position
         
         # will update once walk gets generated
         self.add_interval = None 
@@ -13,16 +13,16 @@ class Walk():
         self.float_steps = False 
 
     # view current walk path
-    def view_walk(self):
+    def view_walk(self) -> list:
         return self.current_walk
     
     # get current walk size
-    def get_size(self):
+    def get_size(self) -> int:
         return self.size
     
     # generate walk beginning at current value
     def generate_walk(self, num_steps: int, add_interval: list = [1, 1], 
-                      add_probability: float = 0.5, uniform: bool = False):
+                      add_probability: float = 0.5, uniform: bool = False) -> None:
         '''Custom add_interval format: [0, x]'''
         # update walk characteristics
         self.size += num_steps
@@ -30,38 +30,41 @@ class Walk():
         self.add_probability = add_probability
         self.float_steps = uniform
 
+        # for validation
+        count_over, count_under = 0, 0
         # generate walk
         for step in range(num_steps):
             # get +/- decision
-            decision = R.uniform(0,1)
+            decision = rd.uniform(0,1)
 
             # if interval subset Z
             if (uniform):
                 # generate random +/- value
-                random_val = R.uniform(add_interval[0], add_interval[1])
+                random_val = rd.uniform(add_interval[0], add_interval[1])
                 # if decision within add_probability, add radom value
                 # else, subtract
                 if decision<add_probability:
+                    count_under += 1
                     self.current_walk.append(self.current_value + random_val)
                     self.current_value = self.current_walk[-1]
                 else:
+                    count_over += 1
                     self.current_walk.append(self.current_value - random_val)
                     self.current_value = self.current_walk[-1]
 
             # if interval subset R
             if (not uniform):
-                random_val = R.randint(add_interval[0], add_interval[1])
+                random_val = rd.randint(add_interval[0], add_interval[1])
                 if decision<add_probability:
                     self.current_walk.append(self.current_value + random_val)
                     self.current_value = self.current_walk[-1]
-                    print(f"Adding {random_val}")
                 else:
                     self.current_walk.append(self.current_value - random_val) 
                     self.current_value = self.current_walk[-1]
-                    print(f"Subtracting {random_val}")
+        
 
     # re-generate walk beginning at start value
-    def regenerate_walk(self):
+    def regenerate_walk(self) -> None:
         # save previous values
         last_walk_steps = self.size - 1
 
@@ -76,24 +79,30 @@ class Walk():
                            add_probability = self.add_probability,
                            uniform = self.float_steps)
 
-    # plot current walk
-    def plot_walk(self, show_sma: bool = False, sma_period: int = None, 
-                  show_running_variance: bool = False):
+    # plot current walk and walk attributes
+    def plot_walk(self, linemarker: bool = True,
+                  show_sma: bool = False, 
+                  sma_period: int = None, 
+                  show_running_variance: bool = False, 
+                  show_volatility: bool = False) -> None:
+        
+        # plot walk
         X = sh.generate_X(self.size)
         Y = self.current_walk 
-        plt.plot(X, Y, marker='o')
+        if linemarker: plt.plot(X, Y, color='steelblue', marker='o', label="Random Walk")
+        if not linemarker: plt.plot(X, Y, color='steelblue', label="Random Walk")
 
         # plot moving average
         if (show_sma):
             # generate (X: period accounted, Y: average for period)
             sma_tuple = sh.sma(self.current_walk, sma_period)
-
+            
             # pull X, Y independently
             sma_X = sma_tuple[0] 
             sma_Y = sma_tuple[1]
-            
+
             # plot moving average
-            plt.plot(sma_X, sma_Y, color = "green", label="Simple Moving Average")
+            plt.plot(sma_X, sma_Y, color = "turquoise", label="Simple Moving Average")
         
         # plot running variance
         if (show_running_variance):
@@ -105,16 +114,21 @@ class Walk():
                 # calculate current variance
                 var_list.append(sh.variance(seen))
             # plot running variance
-            plt.plot(X, var_list, color="purple", label="Variance")
+            plt.plot(X, var_list, color="mediumvioletred", label="Variance")
 
+        # plot volatility
+        if (show_volatility):
+            vol_list = []
+            seen = []
+            for step in self.current_walk:
+                seen.append(step)
+
+                # calculate current volatility = sqrt(variance)
+                vol_list.append(math.sqrt(sh.variance(seen)))
+            # plot running volatility (sd)
+            plt.plot(X, vol_list, color="darkviolet", label="Volatility (sd)")
+
+        plt.legend()
         plt.show()
 
 
-# testing ------------------------------------------------------------------
-walk1 = Walk(5)
-walk1.generate_walk(10)
-print(walk1.get_size())
-print(walk1.view_walk())
-walk1.plot_walk(show_sma=True, show_running_variance=True, sma_period=2)
-    
-        
