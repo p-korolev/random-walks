24912c9d3abd6884f7049bae8d0c4140c829aec5
Phillip Korolev <p.korolev1@outlook.com>
2025-08-06 15:33:44 -0400

    Update interval.py

--- interval.py
+++ interval.py
@@ -1,30 +1,19 @@
 import numpy as np
 
+from numbers import Real
+
 class Interval():
-    '''
-    Basic object that enables interval set usability and readability.
-
-    **Usage**
-
-    Creating interval [-1, 1]:
-
-    >>> I = Interval(lower_bound=-1, upper_bound=1)
-
-    Accessing lower bound:
-
-    >>> I.get_lower_bound()
-    '''
-
-    def __init__(self, lower_bound: float, upper_bound: float, lower_inclusive: bool = True, upper_inclusive: bool = True):
+    def __init__(self, 
+                 lower_bound: Real, 
+                 upper_bound: Real, 
+                 lower_inclusive: bool = True, 
+                 upper_inclusive: bool = True):
         self.lower_bound = lower_bound
         self.upper_bound = upper_bound
         self.lower_inclusive = lower_inclusive
         self.upper_inclusive = upper_inclusive
 
     def __repr__(self):
-        '''
-        Returns list set display of interval assuming element inclusivity stays True always.
-        '''
         return [self.lower_bound, self.upper_bound]
     
     def __str__(self):
